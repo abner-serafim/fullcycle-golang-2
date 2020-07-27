e203a4f79f9da1cef13c8f49477d7e230c0b0b12
Abner Serafim Arede <link.serafim@gmail.com>
2020-07-26 23:12:58 -0300

    Update main.go

--- src/fullcycle-golang-2/main.go
+++ src/fullcycle-golang-2/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
+func Soma(a int, b int) int {
+        return a + b
+}
+
 func main() {
         fmt.Println(Soma(5, 5))
         fmt.Println("Abner Serafim Arede")
-}
-
-func Soma(a int, b int) int {
-        return a + b
 }
